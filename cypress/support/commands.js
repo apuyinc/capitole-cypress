adc8d2f739044b795220d675dedf2a00aac43521
Jose Luis Apuy <jose-luis.apuy@adp.com>
2019-08-16 11:36:50 +0200

    change test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,15 @@
 Cypress.Commands.add('login', () => {
     cy.visit('/#/login')
     cy.get('input[type="email"]').type('capitole_user@capitole-consulting.com')
-    cy.get('input[type="password"]').type('capitole123{enter}')
+    cy.get('input[type="password"]').type('capitole123').click()
     cy.hash().should('eq', '#/')
 })
 
 Cypress.Commands.add('login_api', () => {
     cy.request({
         method: 'POST',
-        url: 'https://conduit.productionready.io/api/users/login',
+        //url: 'https://conduit.productionready.io/api/users/login',
+        url: 'http://localhost:3000/api/users/login',
         body: {
             user: {
                 email: 'capitole_user@capitole-consulting.com',
